8b5fae1951f45c539b4026b1e54c12dafd6fdcc7
Daniel Viola <daniel.viola001@mymdc.net>
2023-06-27 13:43:39 -0400

    potential update?

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const lifeSentenceElement = document.getElementById("life-sentence");
-  const currentDate = new Date();
-  const birthDate = new Date(2001, 1, 3);
-  const diffInMilliseconds = currentDate - birthDate;
-  const diffInYears = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24 * 365));
-  lifeSentenceElement.textContent = `I am just a dumb ${diffInYears}-year-old girl who is trying to figure out this whole "life" thing.`;
-});
+    const lifeSentenceElement = document.getElementById("life-sentence");
+    const currentDate = new Date();
+    const birthDate = new Date(2001, 1, 3);
+    const diffInMilliseconds = currentDate - birthDate;
+    const diffInYears = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24 * 365));
+    lifeSentenceElement.textContent = `I am just a dumb ${diffInYears}-year-old girl who is trying to figure out this whole "life" thing.`;
+  });
